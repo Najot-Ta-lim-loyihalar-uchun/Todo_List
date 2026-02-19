b648cc24fa8c9e312888490f399eab41ffdc5ce0
Murodiljon07 <murodiljonabdumutalovtg1@gmail.com>
2026-02-19 12:06:48 +0500

    finished

--- src/components/Modal.jsx
+++ src/components/Modal.jsx
@@ -2,6 +2,7 @@ import React, { useRef } from "react";
 import { useDispatch } from "react-redux";
 import { newTodo } from "../toolkit/features/todoesSlice";
 import { toast } from "react-toastify";
+
 function Modal({ title, hide, setHide }) {
   const dispatch = useDispatch();
   const todoTitle = useRef();
@@ -18,17 +19,20 @@ function Modal({ title, hide, setHide }) {
   if (hide) return null;
   return (
     <div className="fixed inset-0 bg-black/50 flex items-center justify-center z-50 px-4">
-      <div className="bg-white dark:bg-[#252525] dark:border border-white w-full max-w-md rounded-3xl p-6 flex flex-col gap-6">
-        <h2 className="text-2xl font-bold text-center">{title}</h2>
-        <input
-          ref={todoTitle}
-          type="text"
-          placeholder="Input your note..."
-          className="py-3 px-4 rounded-md border border-[#6C63FF] dark:border-[#fff] outline-none "
-        />
+      <div className="bg-white dark:bg-[#252525] dark:border border-white w-full max-w-md h-[300px] rounded-3xl p-6 flex flex-col justify-between gap-6">
+        <div className="w-full">
+          <h2 className="text-2xl font-bold text-center mb-3">{title}</h2>
+          <input
+            ref={todoTitle}
+            type="text"
+            placeholder="Input your note..."
+            className="py-3 px-4 w-full rounded-md border border-[#6C63FF] dark:border-[#fff] outline-none "
+          />
+        </div>
+
         <div className="flex justify-between">
           <button
-            onClick={() => setHide(false)}
+            onClick={() => setHide(true)}
             className="px-5 py-2 border border-[#6C63FF] text-[#6C63FF] rounded-md cursor-pointer uppercase"
           >
             Cancel
